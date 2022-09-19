4062973ce7aaa6484d4a10930bb6a0552d87e6a4
Mingu Kim <mkim245@gmail.com>
2022-09-19 16:19:47 -0400

    update favorite add remove button

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -1,9 +1,11 @@
 import React from "react";
 import axios from "axios";
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { authContext } from '../providers/AuthProvider';
 
 const Recipe = function ({ singleRecipe, setSingleRecipe }) {
+  const [matchfavorites, setMatchfavorites] = useState(false);
+
   const { auth } = useContext(authContext);
   const handleClick = function (event) {
     event.preventDefault();
@@ -16,8 +18,36 @@ const Recipe = function ({ singleRecipe, setSingleRecipe }) {
     event.target.innerHTML = 'Link Copied!';
   }
 
+  const matchFavorite = function (singleRecipe) {
+    // event.preventDefault()
+    const userString = localStorage.getItem("user")
+    const user = JSON.parse(userString)
+    axios.get(`/favorites/list/${user.id}`)
+      .then((res) => {
+        for (let fav of res.data.favorites) {
+          if (fav.ext_recipe_id === singleRecipe.id) {
+            return setMatchfavorites(true)
+          } else {
+            setMatchfavorites(false)
+          }
+        }
+      })
+      .catch((error) => {
+      });
+  };
+
+  useEffect(() => {
+    matchFavorite(singleRecipe)
+  }, []);
+
   const onCheck = function (e) {
     e.preventDefault();
+
+    // eslint-disable-next-line no-unused-expressions
+    (matchfavorites === true)
+      ? ((e.target.innerHTML = 'Add to Favorite'), setMatchfavorites(false))
+      : ((e.target.innerHTML = 'Remove from Favorite'), setMatchfavorites(true))
+
     axios.post('/favorites', {
       ext_recipe_id: singleRecipe.id,
       ext_recipe_title: singleRecipe.title
@@ -29,6 +59,7 @@ const Recipe = function ({ singleRecipe, setSingleRecipe }) {
         console.log(error.response);
       });
   };
+
   return (
     <div className="single-recipe-page">
       <div className="recipe-title-photo">
@@ -40,11 +71,10 @@ const Recipe = function ({ singleRecipe, setSingleRecipe }) {
         <h5>Servings: {singleRecipe.servings}</h5>
         <h5>Cuisine: {singleRecipe.cuisine}</h5>
         <h5>Dish Type: {singleRecipe.dishType}</h5>
-        {auth ?
-          <h5><form>
-            <label for="favorite" className="favorite"> Mark as Favorite: </label>
-            <input onChange={onCheck} type="checkbox" id="favorite" name="favorite" value="true" />
-          </form></h5>
+        {auth
+          ? (matchfavorites === false)
+            ? <button type="button" style={{width: 190, 'background-color': 'blue'}} className="btn btn-secondary share-recipe-button" onClick={(event) => onCheck(event)}>Add to Favorites</button>
+            : <button type="button" style={{width: 190, 'background-color': 'green'}} className="btn btn-secondary share-recipe-button" onClick={(event) => onCheck(event)}>Remove from Favorites</button>
           : <></>}
       </div>
       <div className="ingredients-instructions">
